bb27e88e67c3c2572b04b80e3435d5b8382a90da
Richard Mak <richardwmak@gmail.com>
2018-02-11 23:06:01 +0100

    worked on adding info to flask session

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@
 
 import datetime
 import time
-from   flask import Flask, request, redirect, url_for, render_template
+from   flask import Flask, request, redirect, url_for, render_template, session
 import logging
 import sys
 
 app = Flask(__name__)
-
+app.secret_key = '\xd5\x99\x98N\x1e\xac7\xc9{\x9d\xdc\xefN\xdf\xcbR\xfc\x8f\xfa$\xa1\xa7\xd7j'
 
 def setup_app():
     # set up logging
@@ -20,15 +20,18 @@ def setup_app():
 
 setup_app()
 
+
 # currently, the way to force a cache refresh to grab updated css files is to
 # pass int(time.time()) and add that to the css file
 
+
 @app.route("/")
 def main_page():
-    if not "username" in request.cookies:
+    if not "username" in session or not "region" in session:
         return redirect(url_for("login"))
     else:
-        pass
+        return redirect(url_for("app"))
+
 
 @app.route("/login")
 def login():
@@ -36,6 +39,21 @@ def login():
     region_list = sorted(["RU", "KR", "BR1", "OC1", "JP1", "NA1", "EUN1", "EUW1", "TR1", "LA1", "LA2"])
     return render_template("login.html", region_list=region_list, timestamp=int(time.time()))
 
+
+@app.route("/set_info", methods=["POST"])
+def set_info():
+    session["username"] = request.form("username")
+    session["region"] = request.form("region")
+
+    return redirect(url_for("app"))
+
+
+@app.route("/app")
+def app():
+    if not "username" in session or not "region" in session:
+        return redirect(url_for("login"))
+    else:
+        pass
 
 
 if __name__ == "__main__":
