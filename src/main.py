43af32e892bae4713fdeaa401b2428ac05b4bd37
Richard Mak <richardwmak@gmail.com>
2018-02-11 09:44:41 +0100

    Implemented first template succesfully (layout/login.html)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-""" Main file """
+""" Main file 
+
+.\virtual\Scripts\python.exe .\src\main.py
+"""
 
 import datetime
-from   flask import Flask
+from   flask import Flask, request, redirect, url_for, render_template
 import logging
 import sys
 
@@ -16,9 +19,18 @@ def setup_app():
 
 setup_app()
 
-@app.route('/')
+@app.route("/")
 def main_page():
-    
+    if not "username" in request.cookies:
+        return redirect(url_for("login"))
+    else:
+        pass
+
+@app.route("/login")
+def login():
+    # hardcoded a list of regions, don't know a better way right now
+    region_list = ["RU", "KR", "BR1", "OC1", "JP1", "NA1", "EUN1", "EUW1", "TR1", "LA1", "LA2"]
+    return render_template("login.html", region_list=region_list)
 
 
 
