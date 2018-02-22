76f3840c12bebad5b3ef5ef406456e6ce7698453
Richard Mak <richardwmak@gmail.com>
2018-02-22 16:35:35 +0100

    Added several linters and made python files compliant

--- src/data.py
+++ src/data.py
@@ -1,4 +1,4 @@
-"""Handles the API requests and data storage
+"""Handles the API requests and data storage.
 
 Test commands:
 
@@ -16,56 +16,51 @@ data1 = data.ApiRequest("reeepicheeep", "EUW1")
 import logging
 import requests
 import sqlite3
-from   typing import Dict
+from   typing import Any, Dict
 
 # set up logging
 logger = logging.getLogger(__name__)
 if not __name__ == "__main__":
     # if not run from main, set up extra logging stuff
     logging.basicConfig(filename='LeagueApp.log', level=logging.INFO)
-    
-class ApiRequest:
-    """"""
-################################################################################
-# INITIALIZATION METHODS
-################################################################################
 
-    def __init__(self, username: str, region: str):
+
+class ApiRequest:
+    """TODO: write docstring."""
+
+###############################################################################
+# INITIALIZATION METHODS
+###############################################################################
+
+    def __init__(self):
         """
-        Initializes various relevant variables (surprise, surprise)
+        Initialize various relevant variables (surprise, surprise).
 
         Args:
             username: League of Legends username
             region: Account region
         """
-
         self.api_key = "RGAPI-e7f9f9d4-20d7-4811-865f-4ce59d0efd04"
         self.version = None
+        self.account_data = []
 
         # self.initialize_db()
         # set_version() both checks the version and run and get_champion_list()
         # if the version is different
         # self.set_version()
 
-
     def init_user(self, username: str, region: str):
-        """
-        Store user info
-        """
+        """Store user info."""
         self.username = username
         self.region = region
 
-    
     def initialise_db(self):
-        """
-        Creates an empty database with all relevant tables if they don't exist
-        """
-
+        """Create empty database with relevant tables if they don't exist."""
         table_dict = {}
         table_dict['info'] = ['version TEXT PRIMARY KEY ASC']
         table_dict['champion'] = ['key INTEGER PRIMARY KEY', 'name TEXT']
 
-################################################################################
+###############################################################################
         """
         Table name: info
         Columns:
@@ -81,7 +76,7 @@ class ApiRequest:
                 )
             """
         Db.query(sql)
-################################################################################
+###############################################################################
         """
         Table name: champion
         Columns:
@@ -106,13 +101,10 @@ class ApiRequest:
                 )
             """
         Db.query(sql)
-################################################################################
-
+###############################################################################
 
     def update_league_version(self):
-        """
-        Check if the current league version matches the stored one
-        """
+        """Check if the current league version matches the stored one."""
         # get stored value
         sql = """
             SELECT
@@ -125,10 +117,11 @@ class ApiRequest:
         local_version = Db.query(sql)[0][0]
 
         # get current value
-        # https://ddragon.leagueoflegends.com/api/versions.json is a list of all
-        # version with the first entry being the current version
+        # https://ddragon.leagueoflegends.com/api/versions.json is a list of
+        # all version with the first entry being the current version
         try:
-            http_response = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
+            http_response = requests.get(
+                "https://ddragon.leagueoflegends.com/api/versions.json")
             data = http_response.json()
 
         except requests.HTTPError as e:
@@ -146,8 +139,8 @@ class ApiRequest:
         riot_version = data[0]
 
         if riot_version == local_version:
-            # if the versions are the same, we can assume everything has updated
-            # just fine
+            # if the versions are the same, we can assume everything has
+            # updated just fine
             print("LeagueApp is up to date.")
             return
         else:
@@ -173,18 +166,16 @@ class ApiRequest:
 
             result = Db.query(sql, params)
 
-            if result == False:
+            if result is False:
                 print("Version could not be updated.")
             else:
                 print("Version successfully updated.")
 
             self.update_champion()
 
-
     def get_data_from_url(self, url: str):
         """
-        Since data is pulled from Riot's API the same way
-        basically every time, this function generalises it
+        Get data from Riot API.
 
         It is always of the form
 
@@ -197,6 +188,7 @@ class ApiRequest:
 
         Returns:
             data: the decoded json data
+
         """
         logger.info("Attempting to connect to Riot API.")
         try:
@@ -209,23 +201,21 @@ class ApiRequest:
             return data
 
         except requests.HTTPError as e:
-            logger.info("An HTTP error has occured: %s" % e.reason)
+            logger.info("An HTTP error has occured: %s" % e)
             return False
 
         except requests.Timeout as e:
 
-            logger.info("Connection timed out: %s" % e.reason)
+            logger.info("Connection timed out: %s" % e)
             return False
 
         except requests.RequestException as e:
-            logger.info("Error processing HTTP request: %s" % e.reason)
+            logger.info("Error processing HTTP request: %s" % e)
             return False
-
 
     def update_champion(self):
         """
-        Method that gets champion names and IDs and puts them into the champion
-        table
+        Get champion names and IDs and puts them into the champion table.
 
         Args:
 
@@ -252,8 +242,6 @@ class ApiRequest:
             riot_champion_list[curr_key]["id"] = curr_info["id"]
             riot_champion_list[curr_key]["title"] = curr_info["title"]
 
-        values = []
-
         sql = """
             SELECT
                 "key"
@@ -265,7 +253,7 @@ class ApiRequest:
         # is the actual champion key
         temp_champion_list = Db.query(sql)
         local_champion_list = []
-        
+
         for key in temp_champion_list:
             local_champion_list.append(key[0])
 
@@ -273,7 +261,7 @@ class ApiRequest:
 
         # list the champions that haven't been updated
         for key in riot_champion_list:
-            if not key in local_champion_list:
+            if key not in local_champion_list:
                 key_diff.append(key)
 
         iter_total = len(key_diff)
@@ -297,70 +285,73 @@ class ApiRequest:
 
             Db.query(sql, params)
 
-            print("Updated ", iter_curr, "/", iter_total, " champions updated.")
+            print("Updated ",
+                  iter_curr, "/",
+                  iter_total,
+                  " champions updated.")
             iter_curr += 1
-
 
     def get_account_data(self) -> str:
         """
-        Method to get account information for a user
+        Get account information for a user.
 
         Args:
             self.username
 
         Returns:
             account_data: dictionary with account data
+
         """
-        data = self.get_data_from_url("summoner/v3/summoners/by-name/"
-            + self.username)
+        data = self.get_data_from_url("summoner/v3/summoners/by-name/" +
+                                      self.username)
 
         return data
 
 
-################################################################################
+###############################################################################
 # INITIALIZATION METHODS OVER
-################################################################################
+###############################################################################
 
-    def get_champion_mastery(self) -> str:
+    def get_champion_mastery(self) -> Dict[Any, Any]:
         """
-        Method that returns all data related to champion mastery
+        Return all data related to champion mastery.
 
         Args:
             self.account_data["id"]
 
         Returns:
             mastery_data - a dict with key champion ID
-        """
 
-        data = self.get_data_from_url("champion-mastery/v3/champion-masteries/by-summoner/"
-            + str(self.account_data["id"]))
+        """
+        data = self.get_data_from_url(
+            "champion-mastery/v3/champion-masteries/by-summoner/" +
+            str(self.account_data["id"]))
 
         # data given is not ordered by key, unfortunately
-        mastery_data = {}
+        mastery_data = {}  # type: Dict
         for champion in data:
-            championId = champion["championId"]
-            mastery_data[championId] = {}
+            champion_id = champion["champion_id"]
+            mastery_data[champion_id] = {}
             for stat in champion:
                 # cut out stats we have already
-                if not stat == "championId" or stat == "playerId":
-                    mastery_data[championId][stat] = champion[stat]
-        
-        return mastery_data
+                if not stat == "champion_id" or stat == "playerId":
+                    mastery_data[champion_id][stat] = champion[stat]
 
+        return mastery_data
 
     def get_rank_data(self) -> str:
         """
-        Method that returns all data related to the users current rank
+        Return all data related to the users current rank.
 
         Args:
             self.account_data["id"]
 
         Returns:
             rank_data - dict with information on ranked things
-        """
 
-        data = self.get_data_from_url("league/v3/positions/by-summoner/"
-            + str(self.account_data["id"]))
+        """
+        data = self.get_data_from_url("league/v3/positions/by-summoner/" +
+                                      str(self.account_data["id"]))
 
         # this returns an array with one entry so just
         # return the dict in there
@@ -369,13 +360,14 @@ class ApiRequest:
 
 
 class Db:
-    """
-    The database class that handles opening/closing db connections, and running
-    and returning query results.
-    """
-    def query(query = "": str, params = {}: Dict[str, str], db_name="league_data.db": str):
+    """Handle db connections/ return query results."""
+
+    def query(self,
+              query: str="",
+              params: Dict[str, str]={},
+              db_name: str="league_data.db"):
         """
-        Run a query (also open and close the connection)
+        Run a query (also open and close the connection).
 
         Args:
             query - the query
@@ -386,11 +378,11 @@ class Db:
             result: the query result as a dict of dicts
                 each subdict is a row
                 note: result == [] if sqlite finds no rows
-        """
 
+        """
         if not db_name.endswith(".db"):
             db_name += ".db"
-        
+
         logger.info("Accessing database: %s" % (db_name))
 
         def dict_factory(cursor, row):
@@ -411,10 +403,9 @@ class Db:
             # close the connection
         except sqlite3.Error as e:
             logger.info("A database error has occurred: ", e.args[0])
-            result = False
-        finally:
-            if not result == False:
-                logger.info("Successfully ran query.")
-            logger.info("Database connection closed.")
-            conn.close()
-            return result
+            return False
+
+        logger.info("Successfully ran query.")
+        logger.info("Database connection closed.")
+        conn.close()
+        return result
--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
-""" Main file 
+r"""
+Main file.
 
 .\virtual\Scripts\python.exe .\src\main.py
 """
-
 from   data import ApiRequest
 import datetime
 import time
 from   flask import Flask, request, redirect, url_for, render_template, session
 import logging
-import sys
 
 app = Flask(__name__)
-app.secret_key = '\xd5\x99\x98N\x1e\xac7\xc9{\x9d\xdc\xefN\xdf\xcbR\xfc\x8f\xfa$\xa1\xa7\xd7j'
+app.secret_key = ('\xd5\x99\x98N\x1e\xac7\xc9{\x9d\xdc\xefN\xdf\xcbR\xfc\x8f' +
+                  '\xfa$\xa1\xa7\xd7j')
+
 
 def setup_app():
-    # set up logging
+    """Set up logging."""
     logging.basicConfig(filename='LeagueApp.log', level=logging.INFO)
     logging.info("\nSTART UP")
     logging.info("Date/time: %s\n" % (datetime.datetime.now()))
+
 
 setup_app()
 
@@ -28,7 +30,8 @@ setup_app()
 
 @app.route("/")
 def main_page():
-    if not "username" in session or not "region" in session:
+    """Route to login or main screen."""
+    if "username" not in session or "region" not in session:
         return redirect(url_for("login"))
     else:
         return redirect(url_for("stats"))
@@ -36,14 +39,19 @@ def main_page():
 
 @app.route("/login")
 def login():
+    """Request user info."""
     # hardcoded a list of regions, don't know a better way right now
     # TODO: improve server names
-    region_list = sorted(["RU", "KR", "BR1", "OC1", "JP1", "NA1", "EUN1", "EUW1", "TR1", "LA1", "LA2"])
-    return render_template("login.html", region_list=region_list, timestamp=int(time.time()))
+    region_list = sorted(["RU", "KR", "BR1", "OC1", "JP1", "NA1",
+                          "EUN1", "EUW1", "TR1", "LA1", "LA2"])
+    return render_template("login.html",
+                           region_list=region_list,
+                           timestamp=int(time.time()))
 
 
 @app.route("/set_info", methods=["POST"])
 def set_info():
+    """Get the info from POST."""
     session["username"] = request.form("username")
     session["region"] = request.form("region")
 
@@ -52,10 +60,13 @@ def set_info():
 
 @app.route("/stats")
 def stats():
-    if not "username" in session or not "region" in session:
+    """Load main page."""
+    if "username" not in session or "region" not in session:
         return redirect(url_for("login"))
-    else:
-        pass
+
+    data = ApiRequest()
+
+    data.init_user(session["username"], session["region"])
 
 
 if __name__ == "__main__":
