e424808a0348fcae3ea637a84ff3e5c21e85cd5c
Richard Mak <richardwmak@gmail.com>
2018-03-23 10:29:44 +0100

    gui update

--- lfm_vis/view/static/js/get_info.js
+++ lfm_vis/view/static/js/get_info.js
@@ -3,5 +3,21 @@
 
 $(document).ready(function()
 {
-    var curr_progress = 0;
+    $("#start-btn").on("click", function()
+    {
+        start($(this));
+    });
 });
+
+function start(btn)
+{
+    if(! btn.hasClass("disabled"))
+    {
+        var circle;
+        btn.addClass("disabled");
+        btn.children("fa-download").css("background-color", "red");
+        // circle = btn.children("fa-circle");
+        // circle.removeClass("fa-cirle");
+        // circle.addClass("fa-circle-notch");
+    }
+}
